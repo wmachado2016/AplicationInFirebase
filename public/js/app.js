ae216123c7354cf51377a9f1d7aebcdafc787003
wanderley <2008.wsm@gmail.com>
2019-05-29 23:00:17 -0300

    Aplicação node em firebase

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+const config = {
+    apiKey: "AIzaSyD5lA98XUDQq3tDdLOabwaYDkQFvp1fg9I",
+    authDomain: "curso-fb-42475.firebaseapp.com",
+    databaseURL: "https://curso-fb-42475.firebaseio.com",
+    projectId: "curso-fb-42475",
+    storageBucket: "curso-fb-42475.appspot.com",
+    messagingSenderId: "599213686271"
+  };
+  firebase.initializeApp(config);
